43cb1ffb215e0761fdf26d1a7a6143bd52f377e3
skmajumder <skmajumder.cse@gmail.com>
2023-10-21 02:56:02 +0600

    color variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./pages/AppLayout/AppLayout";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Breadcrumb/Breadcrumb.jsx
+++ src/components/Breadcrumb/Breadcrumb.jsx
@@ -1,9 +1,21 @@
+import Header from "../Header/Header";
+import styles from "./Breadcrumb.module.css";
+
 const Breadcrumb = () => {
-    return (
-        <div>
-            Breadcrumb
+  return (
+    <Header>
+      <div aria-label="breadcrumb" className="container pt-5 my-5">
+        <div className="row">
+          <div className="col-lg-8 offset-lg-2 text-center">
+            <h1 className={styles.breadcrumbHeadline}>Spaceflight details</h1>
+            <p className={styles.breadcrumbText}>
+              Find out the elaborate features of all the past big spaceflights.
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </Header>
+  );
 };
 
 export default Breadcrumb;
