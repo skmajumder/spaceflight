49e7be16033010affa2746673d8e065ad14ee871
skmajumder <skmajumder.cse@gmail.com>
2023-10-21 20:49:52 +0600

    Pagination

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,41 @@
+const Pagination = () => {
+  return (
+    <section className="container">
+      <div className="row justify-content-center">
+        <div className="col-lg-12">
+          <nav aria-label="Page navigation">
+            <ul className="pagination justify-content-center align-items-center">
+              <li className="page-item">
+                <a className="page-link" href="#" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  1
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  2
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#">
+                  3
+                </a>
+              </li>
+              <li className="page-item">
+                <a className="page-link" href="#" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Pagination;
--- src/pages/AppLayout/AppLayout.jsx
+++ src/pages/AppLayout/AppLayout.jsx
@@ -2,6 +2,7 @@ import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
 import Checkbox from "../../components/Checkbox/Checkbox";
 import Filters from "../../components/Filters/Filters";
 import Footer from "../../components/Footer/Footer";
+import Pagination from "../../components/Pagination/Pagination";
 import SpaceflightList from "../../components/SpaceflightList/SpaceflightList";
 
 const AppLayout = () => {
@@ -17,6 +18,7 @@ const AppLayout = () => {
       </section>
       <Filters />
       <SpaceflightList />
+      <Pagination />
       <Footer>Created by the brilliant minds behind SpaceX</Footer>
     </>
   );
